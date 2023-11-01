09f8482bd0b7b2b2ff415a7d682ff9ce9fa4f833
joaquim <j_vianna07@hotmail.com>
2023-11-01 23:24:59 +0200

    updated to final version

--- task0/exercicio2.py
+++ task0/exercicio2.py
@@ -1,2 +1,55 @@
 # EXERCICIO 2
+'''
+Implemente um autômato finito que reconheça todas as ocorrências da palavra computador
+no texto T. O programa deve apontar em quais posições ocorreram o casamento exato da
+palavra.
+'''
 
+texto ="O computador é uma máquina capaz de variados tipos de tratamento automático de informações ou processamento de dados. Entende-se por computador um sistema físico que realiza algum tipo de computação. Assumiu-se que os computadores pessoais e laptops são ícones da era da informação. O primeiro computador eletromecânico foi construído por Konrad Zuse (1910–1995). Atualmente, um microcomputador é também chamado computador pessoal ou ainda computador doméstico."
+padrao ='computador'
+
+
+def casamento_exato (texto, padrao, exato=False):
+    
+    #PROCESSAMENTO DO PADRAO
+    if exato:
+        padrao =' '+padrao+' ' # Modifica o padrao para casos em que casamento exato = True
+   
+    mascara_de_bits ={} # Inicialização das máscaras
+    m =len(padrao) # Obtem o comprimento do padrao
+    
+    
+    for i in range(m):
+        if padrao[i] not in mascara_de_bits:
+            mascara_de_bits[padrao[i]] = 1 << (m - 1 - i)
+        else:
+            mascara_de_bits[padrao[i]] |= 1 << (m - 1 - i)
+            # atualiza mascara de bits para carecteres repetidos
+ 
+
+    # EXECUCAO DO ALGORITMO    
+    # Inicialização do vetor de bits e das posicoes
+    n =len(texto) # Obtem o comprimento do texto
+    R =0
+    palavra_fixa =1<<(m-1) # Representa a palavra fixa 10^(m-1)  
+    posicoes =[]
+
+    for i in range(n):
+        R = ((R >> 1) | palavra_fixa) & mascara_de_bits.get(texto[i], 0) 
+        # Metodo get(x,0) busca o item na posicao x e se nao tiver retorna 0
+        
+        if R & 1: # Verifica se o vector encontrado e do tipo 0^(m)1
+            if exato:
+                posicoes.append(i-m+2)
+            else:
+                posicoes.append(i-m+1)
+
+    return posicoes
+
+
+# Execucao do codigo
+# casamento_exato (texto, padrao)
+# Imprime [2, 133, 218, 294, 384, 412, 440]
+
+casamento_exato (texto, padrao, True)
+# Imprime [2, 133, 294, 412, 440]
--- task0/exercicio3.py
+++ task0/exercicio3.py
@@ -1,1 +1,106 @@
 # EXERCICIO 3
+
+'''
+Implemente um transdutor finito (máquina de Moore ou Mealy) que, dada uma sequência de
+moedas de 25 e 50 centavos e de 1 real, forneça uma lata de refrigerante quando a sequência
+totalizar 1 real ou mais. Cada moeda inserida deverá corresponder a uma de duas saídas: 0, se
+uma lata não pode ser (ainda) liberada, ou 1, se uma lata deve ser liberada.
+'''
+
+# Definindo as transições (estado, entrada) -> próximo estado
+transicoes={
+    ('q0',25):'q1',
+    ('q0',50):'q2',
+    ('q0',100):'q3',
+    
+    ('q1',25):'q2',
+    ('q1',50):'q4',
+    ('q1',100):'q5',
+    
+    ('q2',25):'q4',
+    ('q2',50):'q3',
+    ('q2',100):'q6', 
+    
+    ('q3',25):'q1',
+    ('q3',50):'q2',
+    ('q3',100):'q3',
+    
+    ('q4',25):'q3',
+    ('q4',50):'q5',
+    ('q4',100):'q7',
+    
+    ('q5',25):'q2',
+    ('q5',50):'q4',
+    ('q5',100):'q5',
+    
+    ('q6',25):'q4',
+    ('q6',50):'q3',
+    ('q6',100):'q6',
+    
+    ('q7',25):'q3',
+    ('q7',50):'q5',
+    ('q7',100):'q7'
+    
+}
+
+# Definindo as saídas (estado, entrada) -> saída
+saidas ={
+    ('q0',25):0,
+    ('q0',50):0,
+    ('q0',100):1,
+    
+    ('q1',25):0,
+    ('q1',50):0,
+    ('q1',100):1,
+    
+    ('q2',25):0,
+    ('q2',50):1,
+    ('q2',100):1,
+    
+    ('q3',25):0,
+    ('q3',50):0,
+    ('q3',100):1,
+    
+    ('q4',25):1,
+    ('q4',50):1,
+    ('q4',100):1,
+    
+    ('q5',25):0,
+    ('q5',50):0,
+    ('q5',100):1,
+    
+    ('q6',25):0,
+    ('q6',50):1,
+    ('q6',100):1,
+    
+    ('q7',25):1,
+    ('q7',50):1,
+    ('q7',100):1
+}
+
+# Definindo o estado inicial
+e_inicial ='q0'
+
+
+# Definindo a função Máquina de Refri
+def maquina_refrigerante(transicoes, saidas, e_inicial, moedas):
+    estado =e_inicial
+    sequencia_de_saida =[]
+    
+    for moeda in moedas:
+        # Mapeia o estado e a moeda de entrada para saber qual o proximo estado
+        novo_estado = transicoes[(estado, moeda)]
+        
+        # Mapeia o estado e a moeda de entrada pra saber qual o simbolo de saida
+        simbolo_de_saida = saidas[(estado, moeda)]
+        
+        estado = novo_estado # Atualiza o estado para novo estado
+        sequencia_de_saida.append(simbolo_de_saida) # Atualiza o simbolo de saida na sequencia de saida
+    
+    return sequencia_de_saida
+
+
+# Executando o código
+moedas=[50,25,50,100,25,50,100] 
+
+print(maquina_refrigerante(transicoes, saidas, e_inicial, moedas)) # Deve imprimir [0, 0, 1, 1, 0, 1, 1]
